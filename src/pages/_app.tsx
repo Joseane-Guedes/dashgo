bf6d866ab5ddfca61034402212abfc345ca2cf7e
Joseane-Guedes <joseane_23@hotmail.com>
2021-08-24 17:23:24 -0300

    config chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+import React from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+//Internet caiu no momento 8:50 da aula 
