d4a040717cafd2e9db080567da3d813ed1a52128
Joseane-Guedes <joseane_23@hotmail.com>
2021-08-24 19:08:57 -0300

    add font config

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -15,6 +15,10 @@ export const theme = extendTheme<Theme>({
             "50": "#EEEEF2"
         }
     },
+    fonts: {
+        heading: 'Roboto',
+        body: 'Roboto',
+    },
     styles: {
         global: {
             body: {
