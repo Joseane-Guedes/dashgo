e9c083eee3217865a205c290c6197f993759b9a8
Joseane-Guedes <joseane_23@hotmail.com>
2021-08-31 20:12:03 -0300

    Add resposivity to components

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,18 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
+interface ProfileProps {
+    showProfileData?: boolean;
+}
 
-export function Profile() {
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center"
-            >
-            <Box mr="4" textAlign="right">
-                <Text> Joseane Guedes </Text>
-                <Text color="gray.300" fontSize="small" > 
-                joseane_23@hotmail.com
-                </Text>
-            </Box>
+        >
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text> Joseane Guedes </Text>
+                    <Text color="gray.300" fontSize="small" >
+                        joseane_23@hotmail.com
+                    </Text>
+                </Box>)}
 
             <Avatar size="md" name="Joseane Guedes" src="https://github.com/Joseane-Guedes.png" />
-            </Flex>
+        </Flex>
     );
 }
