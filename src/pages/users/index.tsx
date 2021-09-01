1b6ac746a1b8197dadf56e0cc7cd62a9c11824aa
Joseane-Guedes <joseane_23@hotmail.com>
2021-09-01 18:58:34 -0300

    Add Responsive Table

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,120 +1,115 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import React from "react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
-import { NotificationsNav } from "../../components/Header/NotificationsNav";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-    return (
-        <Box>
-            <Header />
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
 
-            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6" >
-                <Sidebar />
+  return (
+    <Box>
+      <Header />
 
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
-                        <Button
-                            as="a"
-                            size="sm"
-                            fontSize="sm"
-                            colorScheme="pink"
-                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-                        >
-                            Criar novo
-                        </Button>
-                    </Flex>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px="6" color="gray.300" widht="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                <Th> Data de cadastro </Th>
-                                <Th width="8"> </Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Joseane Guedes</Text>
-                                        <Text fontSize="sm" color="gray.300">joseane_23@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td> 30 de Agosto, 2021</Td>
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button></Td>
-                            </Tr>
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Joseane Guedes</Text>
-                                        <Text fontSize="sm" color="gray.300">joseane_23@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td> 30 de Agosto, 2021</Td>
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button></Td>
-                            </Tr>
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Joseane Guedes</Text>
-                                        <Text fontSize="sm" color="gray.300">joseane_23@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td> 30 de Agosto, 2021</Td>
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button></Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px={["4", "4", "6"]} color="gray.300" widht="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                {isWideVersion && <Th> Data de cadastro </Th>}
+                <Th width="8"> </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Joseane Guedes</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      joseane_23@hotmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td> 30 de Agosto, 2021</Td>}
+              </Tr>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Joseane Guedes</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      joseane_23@hotmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td> 30 de Agosto, 2021</Td>}
+              </Tr>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Joseane Guedes</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      joseane_23@hotmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td> 30 de Agosto, 2021</Td>}
+              </Tr>
+            </Tbody>
+          </Table>
 
-                    <NotificationsNav />
-                    <Pagination />
-                </Box>
-            </Flex>
+          <Pagination />
         </Box>
-    );
+      </Flex>
+    </Box>
+  );
 }
