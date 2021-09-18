6547048d1a831f6b5f357628da79a94c5da76684
Joseane-Guedes <joseane_23@hotmail.com>
2021-09-18 12:40:29 -0300

    React Query: Config

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -18,22 +19,26 @@ import Link from "next/link";
 import React from "react";
 import { useEffect } from "react";
 import { RiAddLine } from "react-icons/ri";
+import { useQuery } from 'react-query'
+
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const { data, isLoading, error} = useQuery ('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+        
+    return data;
+    })
+
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log('so jesus na causa', data));
-  }, [])
-
 
   return (
     <Box>
@@ -60,7 +65,17 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
+         { isLoading ? (
+           <Flex justify="center">
+            <Spinner />
+           </Flex>
+        ) : error ? (
+            <Flex justify="center">
+              <Text> Falha ao obter dados dos usuários.</Text>
+            </Flex>
+        ) : (
+          <>
+           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
                 <Th px={["4", "4", "6"]} color="gray.300" widht="8">
@@ -118,6 +133,9 @@ export default function UserList() {
           </Table>
 
           <Pagination />
+          </>
+         )}
+
         </Box>
       </Flex>
     </Box>
